bf80828235c9d65f1259515b0e123be0380ac925
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-10-17 00:22:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+print("*** WELCOME TO THE LIBRARY***")
+
+catalogue = [
+    "The Lord of the Rings",
+ "Harry Potter",
+  "Hunger Games", "The Lion, the Witch, and the Wardrobe",
+   "Becoming", 
+   "I am Malala",
+    "Cooking for Dummies", 
+    "The Joy of Cooking"
+]
+
+
+fantasy = [
+"The Lord of the Rings",
+ "Harry Potter",
+  "Hunger Games",
+   "The Lion, the Witch, and the Wardrobe"
+
+]
+
+non_fiction = [
+    "Becoming", 
+   "I am Malala"
+]
+
+cooking = [
+     "Cooking for Dummies", 
+    "The Joy of Cooking"
+]
+
+genres = ["FANTASY", "NON-FICTION", "COOKING"]
+
+user_activity = input("What would you like to do? \n [A]Browse [B] Checkout book \n")
+
+if user_activity == "A":
+    print("Here are all of our book genres: ")
+    for i in genres:
+        print(i)
+
+genre_select = input("Great. What genre would you like to see?").upper()
+
+while genre_select not in genres:
+    genre_select = input("Not a genre. what genre would you like to see in our catalogue \n").upper()
+
+if genre_select == "FANTASY":
+        for i in fantasy:
+            print(i)
+elif genre_select == "NON-FICTION":
+        for i in non_fiction:
+            print(i)
+elif genre_select == "COOKING":
+        for i in cooking:
+            print(i)
